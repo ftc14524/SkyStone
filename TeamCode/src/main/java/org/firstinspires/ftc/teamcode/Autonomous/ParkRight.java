13674dd4ffe713e6a8fa48a0a2ff281b93715a9e
heroineforever <hanaasiddiqui@yahoo.com>
2020-01-23 17:35:37 -0600

    Made ParkLeft and ParkRight classes from Left and Right ~HS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ParkRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ParkRight.java
@@ -4,18 +4,15 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name = "TEST Right", group = "Autonomous")
+@Autonomous(name = "Parking Right", group = "Autonomous")
 
-public class Right extends org.firstinspires.ftc.teamcode.Autonomous.AutonomousRight {
+public class ParkRight extends org.firstinspires.ftc.teamcode.Autonomous.Autonomous {
 
 
     @Override
     public void runOpMode() {
 
         super.runOpMode();
-
-        platform.setPosition(Servo.MIN_POSITION);
-        armPivot.setPosition(Servo.MAX_POSITION);
         waitForStart();
         strafe(0, 1, 1, 1.5);
         stop();
