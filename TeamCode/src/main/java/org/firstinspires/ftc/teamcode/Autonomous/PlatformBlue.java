ecbb88dc616b224f6eef308c1e8c1fd6fb3cc848
Akshyat Dumka <akshyatdumka@gmail.com>
2020-02-11 16:47:24 -0600

    IJ - Platform Blue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PlatformBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PlatformBlue.java
@@ -13,20 +13,20 @@ public class PlatformBlue extends org.firstinspires.ftc.teamcode.Autonomous.Auto
     @Override
     public void runOpMode() {
 
-        super.runOpMode();
         waitForStart();
-        strafe(1, 1, 2.4);
+        strafe(1, 1, 2.5);
         waitFor(1);
         platform.setPosition(Servo.MIN_POSITION);
         waitFor(1);
-        strafe(-1, 0, 1.3);
+        strafe(-1, -0.8, 3);
         waitFor(1);
-        rotate(false, 2, 1);
+        rotate(true, 2.1, 1);
         waitFor(1);
         platform.setPosition(Servo.MAX_POSITION);
         waitFor(1);
-        strafe(1, 0, 4);
-        strafe(1, 1, 1);
+        strafe(1, 0, 2);
+        waitFor(1);
+        strafe(-1, 0,3.3);
         stop();
     }
 }
